65d1921102ccebda9c940397f6dba2c9b51d48a4
krowpu <krowpu@tightmail.com>
2017-03-18 22:18:13 +0500

    Register Capybara driver `:scrapod`

--- lib/scrapod.rb
+++ lib/scrapod.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 require 'scrapod/version'
+require 'scrapod/driver'
+
+require 'capybara'
+require 'capybara/webkit/configuration'
 
 ##
 # A driver of remote headless scraping cluster for Capybara
 # (aka remote Capybara Webkit).
 #
 module Scrapod
+end
+
+Capybara.register_driver :scrapod do |app|
+  Scrapod::Driver.new app, Capybara::Webkit::Configuration.to_hash
 end
